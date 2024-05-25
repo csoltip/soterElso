7e4abae9d18ae917000516e7a83e8affe853acc2
CsoltiP <csolti@szamalk-szalezi.hu>
2024-05-25 08:57:51 +0200

    void sta_meth()

--- src/lokalisrepo/LokalisRepo.java
+++ src/lokalisrepo/LokalisRepo.java
@@ -3,7 +3,11 @@ package lokalisrepo;
 public class LokalisRepo {
 
     public static void main(String[] args) {
-        
+        sta_meth();
+    }
+
+    private static void sta_meth() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
