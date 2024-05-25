df052edcf137f8a187c9a040b7d322f2168d9b2b
CsoltiP <csolti@szamalk-szalezi.hu>
2024-05-25 08:58:28 +0200

    impl void sta_meth()

--- src/lokalisrepo/LokalisRepo.java
+++ src/lokalisrepo/LokalisRepo.java
@@ -7,7 +7,7 @@ public class LokalisRepo {
     }
 
     private static void sta_meth() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
     }
     
 }
